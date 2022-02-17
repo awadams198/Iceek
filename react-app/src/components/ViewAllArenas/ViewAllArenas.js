e8222b1798156a75a48ced6e574226f60dcf7cb0
Your Name <awadams198@gmail.com>
2022-02-17 18:43:06 -0500

    testing

--- react-app/src/components/ViewAllArenas/ViewAllArenas.js
+++ react-app/src/components/ViewAllArenas/ViewAllArenas.js
@@ -43,9 +43,7 @@ function ViewArenas() {
  
                })}
            </div>
-            {/* <div className='berkeley-map-parent-container'>
-               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26361348.14090973!2d-113.75721884302386!3d36.240999911993384!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited%20States!5e0!3m2!1sen!2sus!4v1636756177169!5m2!1sen!2sus" title='allspot' className='berkeley-map'></iframe>
-           </div> */}
+          
        </div>
    )
  }
