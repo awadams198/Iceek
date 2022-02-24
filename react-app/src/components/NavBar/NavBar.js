3f351010682ecfc8d8412f10ed06b46570e44d61
Your Name <awadams198@gmail.com>
2022-02-24 15:23:42 -0500

    resized homepage image, and fixed navbar size

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -77,51 +77,55 @@ const NavBar = ({ isLoaded }) => {
     sessionLinks = (
       <>
         <header>
-          <div className="about-container">
-            <a
-              className="homeAboutLink"
-              href="https://github.com/awadams198"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img
-                src={"https://i.ibb.co/gFT8dHz/logo-github.jpg"}
-                className="about-img"
-              />
-            </a>
-            <a
-              className="homeAboutLink"
-              href="https://www.linkedin.com/in/anthony-adams-a4221a228/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img
-                src={"https://i.ibb.co/S3587Vd/Linked-In-logo-initials.png"}
-                className="about-img"
-              />
-            </a>
-          </div>
           <div className="container">
-            <h1 className="logo"></h1>
-            <nav>
-              <ul className="main-nav">
-                <li href="#">
-                  <NavLink to="/" exact={true} activeClassName="active">
-                    Home
-                  </NavLink>
-                </li>
-                <li href="#">
-                  <NavLink to="/login" exact={true} activeClassName="active">
-                    Login
-                  </NavLink>
-                </li>
-                <li href="#">
-                  <NavLink to="/sign-up" exact={true} activeClassName="active">
-                    Sign Up
-                  </NavLink>
-                </li>
-              </ul>
-            </nav>
+            <div className="about-container">
+              <a
+                className="homeAboutLink"
+                href="https://github.com/awadams198"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src={"https://i.ibb.co/gFT8dHz/logo-github.jpg"}
+                  className="about-img"
+                />
+              </a>
+              <a
+                className="homeAboutLink"
+                href="https://www.linkedin.com/in/anthony-adams-a4221a228/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src={"https://i.ibb.co/S3587Vd/Linked-In-logo-initials.png"}
+                  className="about-img"
+                />
+              </a>
+              <h1 className="logo"></h1>
+              <nav>
+                <ul className="main-nav">
+                  <li href="#">
+                    <NavLink to="/" exact={true} activeClassName="active">
+                      Home
+                    </NavLink>
+                  </li>
+                  <li href="#">
+                    <NavLink to="/login" exact={true} activeClassName="active">
+                      Login
+                    </NavLink>
+                  </li>
+                  <li href="#">
+                    <NavLink
+                      to="/sign-up"
+                      exact={true}
+                      activeClassName="active"
+                    >
+                      Sign Up
+                    </NavLink>
+                  </li>
+                </ul>
+              </nav>
+            </div>
           </div>
         </header>
       </>
